f9a8137c6be181e326826a7642562fd2874e2338
thibault genaitay <tgenaitay@gmail.com>
2016-10-10 15:04:30 +0200

    OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'coaching/answer'
+  get 'answer', to: 'coaching#answer'
+  get 'ask', to: 'coaching#ask'
+
+  get 'coaching/ask'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
